0ce0f6c50714af779d91deb5deab633cdf4626ae
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2026-04-24 01:43:19 +0100

    auth service cors

--- src/index.ts
+++ src/index.ts
@@ -8,32 +8,49 @@ import { handle_frontend } from "./frontend";
 import { handle_logout } from "./logout";
 import { handle_me } from "./me";
 
-import { verify_token } from "./util";
-import { handle_provider_names, provider_friendly_names, provider_names, type ProviderName } from './providers';
-import { handle_discord_activity_auth } from './discord_activity';
-
-const { preflight } = cors({
-    origin: "*", // TODO: restrict to allowed origins
-    allowMethods: "GET",
-    allowHeaders: ["Authorization", "Content-Type"]
-});
-
-const router = AutoRouter({
-    before: [preflight]
-});
-
-router
-    .get("/login", handle_login)
-    .get("/login/:provider", handle_login)
-    .get("/callback/:provider", handle_callback)
-    .get("/logout", handle_logout)
-    .get("/me", handle_me)
-    .get("/providers", handle_provider_names)
-    .get("/", handle_frontend)
-    .post("/login-discord-activity", handle_discord_activity_auth);
+import { validate_origin, verify_token } from "./util";
+import { handle_provider_names, provider_friendly_names, provider_names, type ProviderName } from "./providers";
+import { handle_discord_activity_auth } from "./discord_activity";
 
 export default class AuthService extends WorkerEntrypoint<Env> {
     async fetch(request: Request): Promise<Response> {
+        // note that this is only preflight, the actual CORS headers are set in each handler to allow for dynamic origins
+        const { preflight } = cors({
+            origin: (origin) => {
+                if (!origin || origin === "null") {
+                    return;
+                }
+
+                // allow anything under discordsays
+                if (origin.endsWith(".discordsays.com") || origin === "https://discordsays.com") {
+                    return origin;
+                }
+
+                const {is_allowed} = validate_origin(origin, this.env);
+                if (is_allowed) {
+                    return origin;
+                }
+
+                return;
+            },
+            allowMethods: "GET, POST",
+            allowHeaders: ["Authorization", "Content-Type"]
+        });
+
+        const router = AutoRouter({
+            before: [preflight]
+        });
+
+        router
+            .get("/login", handle_login)
+            .get("/login/:provider", handle_login)
+            .get("/callback/:provider", handle_callback)
+            .get("/logout", handle_logout)
+            .get("/me", handle_me)
+            .get("/providers", handle_provider_names)
+            .get("/", handle_frontend)
+            .post("/login-discord-activity", handle_discord_activity_auth);
+
         return router.fetch(request, this.env, this.ctx);
     }
 
